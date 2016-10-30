d2ca01db6e4d063f5419e458a433c5c03af542de
Conrad <butkevich.siarhei@gmail.com>
2016-10-30 17:26:56 +0300

    -

--- TimesheetsAndCounting/TimesheetsAndCounting.Frontend/scripts/app/controllers/navigationController.js
+++ TimesheetsAndCounting/TimesheetsAndCounting.Frontend/scripts/app/controllers/navigationController.js
@@ -1,6 +1,6 @@
 ﻿(function () {
-    angular.module('app.controllers').controller('navigationController', ['$scope', '$timeout', '$mdSidenav',
-        function ($scope, $timeout, $mdSidenav) {
+    angular.module('app.controllers').controller('navigationController', ['$scope', '$timeout', '$mdSidenav', '$timeout', '$http',
+        function ($scope, $timeout, $mdSidenav, $timeout, $http) {
             $scope.toggleLeft = buildToggler('left');
             $scope.toggleRight = buildToggler('right');
 
@@ -10,29 +10,17 @@
                 };
             }
             
-            var filteredItems = [{
-                id: 1,
-                name: 'Шурпак А.Ф.',
-                day1: 8,
-                day2: 8,
-                day3: 8,
-                day4: 8,
-                day5: 8,
-                day6: 8,
-                day7: 8
-            }, {
-                id: 2,
-                name: 'Волошин В.В.',
-                day1: 8,
-                day2: 8,
-                day3: 8,
-                day4: 8,
-                day5: 8,
-                day6: 8,
-                day7: 8
-            }];
+            
 
-            $scope.getTimesheet = function() {
+            
+
+            $scope.promise = $timeout(function () {
+                $http.get('scripts/app/controllers/timesheet.json').then(function (timesheets) {
+                    $scope.timesheets = timesheets.data;
+                });
+            }, 2000);
+
+            $scope.getTimesheets = function() {
                 
             }
         }]);
