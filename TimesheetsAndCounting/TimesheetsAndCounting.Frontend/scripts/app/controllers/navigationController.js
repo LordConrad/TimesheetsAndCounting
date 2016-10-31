c7cb98c6ce329d55ccb2cf673f9fae1da74f2366
Butkevich Siarhei <sbutkevich@scnsoft.com>
2016-10-31 16:23:49 +0300

    -

--- TimesheetsAndCounting/TimesheetsAndCounting.Frontend/scripts/app/controllers/navigationController.js
+++ TimesheetsAndCounting/TimesheetsAndCounting.Frontend/scripts/app/controllers/navigationController.js
@@ -1,6 +1,6 @@
 ﻿(function () {
-    angular.module('app.controllers').controller('navigationController', ['$scope', '$timeout', '$mdSidenav', '$timeout', '$http',
-        function ($scope, $timeout, $mdSidenav, $timeout, $http) {
+    angular.module('app.controllers').controller('navigationController', ['$scope', '$timeout', '$mdSidenav', '$timeout', '$http', '$mdEditDialog',
+        function ($scope, $timeout, $mdSidenav, $timeout, $http, $mdEditDialog) {
             $scope.toggleLeft = buildToggler('left');
             $scope.toggleRight = buildToggler('right');
 
@@ -15,13 +15,31 @@
             
 
             $scope.promise = $timeout(function () {
-                $http.get('scripts/app/controllers/timesheet.json').then(function (timesheets) {
+                $http.get('timesheet.json').then(function (timesheets) {
                     $scope.timesheets = timesheets.data;
                 });
             }, 2000);
 
             $scope.getTimesheets = function() {
-                
-            }
+
+            };
+
+            $scope.editTimesheet = function(event, person) {
+                var promise = $mdEditDialog.show({
+                    targetEvent: event,
+                    modelValue: person.day3,
+                    save: function(input) {
+                        person.day3 = input.$modelValue;
+                    }
+                });
+
+                promise.then(function(ctrl) {
+                    var input = ctrl.getInput();
+
+                    input.$viewChangeListeners.push(function() {
+                        input.$setValidity('test', input.$modelValue !== 'test');
+                    });
+                });
+            };
         }]);
 })();
---
+++ TimesheetsAndCounting/TimesheetsAndCounting.Frontend/scripts/app/controllers/timesheetOptionsController.js
@@ -0,0 +1,21 @@
+﻿(function() {
+    angular.module('app.controllers', ['$element', '$q', 'save', '$scope',
+        function($element, $q, save, $scope) {
+
+            function update() {
+                if ($scope.editDialog.$invalid) {
+                    return $q.reject();
+                }
+                if (angular.isFunction(save)) {
+                    return $q.when(save($scope.editDialog.input));
+                }
+                return $q.resolve();
+            }
+
+            this.dismiss = function() {
+                $element.remove();
+            };
+
+        }
+    ]);
+})();
